1df63203f95ff118257e6b8989ed8d8c35dc2a35
yutaro113 <yuu.swingshift113@gmail.com>
2019-03-30 16:34:03 +0900

    20190330

---
+++ 01_JAVASCRIPT_20190330/lecture/test.js
@@ -0,0 +1,23 @@
+alert(111);
+console.log("はじめてのJavaScript")
+console.log(23+5)
+console.log(2000-1800)
+console.log("18+5")
+
+var name = "井上 祐太郎"
+console.log(name)
+
+var score = Math.floor(Math.random()*100)+1
+/*var point = 70; */
+
+if (score >= 80){
+    console.log("SCORE:%d 素晴らしい！おめでとう",score)
+}else if (score >= 70){
+    console.log("SCORE:%d まあまあ素晴らしい！おめでとう",score)
+}else if (score >= 60){
+    console.log("SCORE:%d よくできました！おめでとう",score)
+}else if (score >= 50){
+    console.log("SCORE:%d まあまあよくできました！おめでとう",score)
+}else {
+    console.log("SCORE:%d もっと頑張りましょう!",score)
+} 
